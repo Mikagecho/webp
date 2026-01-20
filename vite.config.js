e97525cf9de01333982685719effa81145f27138
Mikagecho <mikage.sakurano@gmail.com>
2026-01-20 16:59:03 +0900

    Add GitHub Pages deployment workflow and fix build config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/canvas/',
-    build: {
-        rollupOptions: {
-            input: {
-                webp: 'webp.html'
-            }
-        }
-    }
+    base: '/webp/',
 })
